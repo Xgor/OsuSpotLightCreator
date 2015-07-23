8f5781a41c816ca74818dacb8140856fc753daf5
Xgor <real.xgor@hotmail.com>
2015-07-23 16:44:57 +0200

    Alpha 0.1
    Currently prints out SB that follow the circles

---
+++ Osu Spotlight Creator/Ous Spotlight Creator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace osu_Spotlight_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.LoadMap = new System.Windows.Forms.Button();
+            this.openMapDialog = new System.Windows.Forms.OpenFileDialog();
+            this.mapTextBox = new System.Windows.Forms.TextBox();
+            this.imgTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(364, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // LoadMap
+            // 
+            this.LoadMap.Location = new System.Drawing.Point(278, 62);
+            this.LoadMap.Name = "LoadMap";
+            this.LoadMap.Size = new System.Drawing.Size(75, 23);
+            this.LoadMap.TabIndex = 2;
+            this.LoadMap.Text = "LoadMap";
+            this.LoadMap.UseVisualStyleBackColor = true;
+            this.LoadMap.Click += new System.EventHandler(this.LoadMap_Click);
+            // 
+            // openMapDialog
+            // 
+            this.openMapDialog.DefaultExt = "osu";
+            this.openMapDialog.FileName = "openMapDialog";
+            // 
+            // mapTextBox
+            // 
+            this.mapTextBox.Location = new System.Drawing.Point(12, 62);
+            this.mapTextBox.Name = "mapTextBox";
+            this.mapTextBox.Size = new System.Drawing.Size(260, 20);
+            this.mapTextBox.TabIndex = 3;
+            this.mapTextBox.TextChanged += new System.EventHandler(this.mapTextBox_TextChanged);
+            // 
+            // imgTextBox
+            // 
+            this.imgTextBox.Location = new System.Drawing.Point(113, 148);
+            this.imgTextBox.Name = "imgTextBox";
+            this.imgTextBox.Size = new System.Drawing.Size(122, 20);
+            this.imgTextBox.TabIndex = 4;
+            this.imgTextBox.TextChanged += new System.EventHandler(this.imgTextBox_TextChanged);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(137, 207);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 5;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 261);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.imgTextBox);
+            this.Controls.Add(this.mapTextBox);
+            this.Controls.Add(this.LoadMap);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "osu!Light Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button LoadMap;
+        private System.Windows.Forms.OpenFileDialog openMapDialog;
+        private System.Windows.Forms.TextBox mapTextBox;
+        private System.Windows.Forms.TextBox imgTextBox;
+        private System.Windows.Forms.Button generateButton;
+
+    }
+}
+
---
+++ Osu Spotlight Creator/Ous Spotlight Creator/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+using System.Globalization;
+
+namespace osu_Spotlight_Creator
+{
+    public partial class Form1 : Form
+    {
+        int xOffset = 48;
+        int yOffset = 64;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        // Ask for the map to add Spotlight for
+        private void LoadMap_Click(object sender, EventArgs e)
+        {
+            openMapDialog.DefaultExt = ".osu";
+            openMapDialog.Filter = "Osu Map files|*.osu";
+            openMapDialog.Multiselect = false;
+            openMapDialog.Title = "Load Map File";
+            openMapDialog.InitialDirectory = Environment.CurrentDirectory;
+            DialogResult result = openMapDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            mapTextBox.Text = openMapDialog.InitialDirectory;
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                using (Stream openFileStream = openMapDialog.OpenFile())
+                {
+                    StreamReader reader = new StreamReader(openFileStream);
+                    
+                    int lineCount = 0;
+                    float SliderMultiplier;
+                    float BPM;
+                    while (!reader.EndOfStream)
+                    {
+                        lineCount++;
+                        String line = reader.ReadLine();
+
+                        // Searches for spesific parts in the osu file to get the data needed
+
+                        // Looks for SliderMultiplier as it's needed for calculating slider speed
+                        if (line.Contains("SliderMultiplier:"))
+                        {
+                            line = line.Replace("SliderMultiplier:", "");
+                            line = line.Replace(".", ",");
+                            SliderMultiplier = floatParser(line);
+                        }
+
+                        // Looks for BPM as it's needed for calculating slider speed
+                        else if (line == "[TimingPoints]")
+                        {
+                            getNextInt(ref reader);
+                            BPM =getNextFloat(ref reader);
+                            reader.ReadLine();
+                        }
+                        // Searches for the Hitobjects and saves the position on every point the spotlight should follow
+                        else if (line == "[HitObjects]")
+                        {
+
+                            Console.WriteLine("Found [HitObjects] at line " + lineCount.ToString() + " !");
+                            
+                            List<Beat> list = new List<Beat>();
+
+                            while (!reader.EndOfStream)
+                            {
+                                Beat beat;
+                                beat.X = (short)(getNextInt(ref reader) + xOffset);
+                                beat.Y = (short)(getNextInt(ref reader) + yOffset);
+                                beat.Time = getNextInt(ref reader);
+                                list.Add(beat);
+                                reader.ReadLine();
+                            }
+
+
+                            // Prints out the storyboard data for the spotlight
+                            Console.WriteLine("Sprite,Foreground,Centre," +imgTextBox.Text.ToString() +",320,320");
+                            for (int i = 1; 1 < list.Count; i++ )
+                            {
+                                Console.WriteLine(" M,0," + list[i - 1].Time.ToString() +"," + list[i].Time.ToString()
+                                    + "," + list[i - 1].X.ToString() +"," + list[i - 1].Y.ToString()
+                                    + "," + list[i].X.ToString() + "," + list[i].Y.ToString());
+                            }
+                            break;
+                        }
+                    }
+              //      Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+            }
+             
+        }
+
+        private int getNextInt(ref StreamReader reader)
+        {
+            String tempString = "0";
+            while((char)reader.Peek() != ',')
+            {
+                tempString += (char)reader.Read();
+            }
+            reader.Read();
+            return int.Parse(tempString);
+        }
+        private float getNextFloat(ref StreamReader reader)
+        {
+            String tempString = "0";
+            while ((char)reader.Peek() != ',')
+            {
+                tempString += (char)reader.Read();
+            }
+            reader.Read();
+            return floatParser(tempString);
+
+        }
+
+        private float floatParser(string floatString)
+        {
+            try
+            {
+                float number = Single.Parse(floatString, CultureInfo.InvariantCulture);
+                Console.WriteLine("{0} -> {1}", floatString, number);
+                return number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("'{0}' is not in a valid format.", floatString);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("{0} is outside the range of a Single.", floatString);
+
+            }
+            return 0;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mapTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    public struct Beat
+    {
+        public short X;
+        public short Y;
+        public int Time;
+    }
+}
